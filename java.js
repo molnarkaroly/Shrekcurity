a2cfb6d28f64c11c601c8f636e5bef521167ad6a
Molnár Károly <118881658+molnarkaroly0@users.noreply.github.com>
2023-09-27 12:28:52 +0200

    TUTUTUTUTUUT MAX VERSTRAPPEN
    TUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPENTUTUTUTUTUUT MAX VERSTRAPPEN

--- java.js
+++ java.js
@@ -7,3 +7,22 @@ function shrekLoad(){
     }
 }
 
+function supportbajod(){
+    var randy = Math.floor(Math.random()*10);
+    if (randy == 2){
+        document.getElementById('agybaj').setAttribute('placeholder', 'Írd ide mi a bajod (az agybajon kívül :D)');
+    }else{
+        document.getElementById('agybaj').setAttribute('placeholder', "Kérem írja le mi aproblémája, és mi probálunk minnél hamarabb segíteni megoldani azt :)");
+    }
+}
+
+function tudastarshrek(){
+    var randy = Math.floor(Math.random()*10);
+    if (randy == 2){
+        document.getElementById('mindentud').textContent="Shrek mindent tud";
+    }else{
+        document.getElementById('mindentud').textContent="Tudástár";
+    }
+}
+
+
