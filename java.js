3be86a4c82042d12279fcd0ebebbd089acc4f000
molnarkaroly <molnar.karoly@diak.szbi-pg.hu>
2023-10-08 19:19:19 +0200

    rev

--- java.js
+++ java.js
@@ -82,3 +82,25 @@ function reveal() {
   }
 
 window.addEventListener("scroll", reveal);
+
+
+
+function reveal_tab() {
+    var reveals = document.querySelectorAll(".reveal_tab");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+window.addEventListener("scroll", reveal_tab);
+
+
